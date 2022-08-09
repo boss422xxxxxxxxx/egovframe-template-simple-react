3af23a84ec74886f45cc5fa64ef97da522b3a903
boss422xxxxxxxxx <boss422@gmail.com>
2022-08-10 06:34:18 +0900

    ww

--- src/egov/common/leftmenu/EgovLeftNavAbout.jsx
+++ src/egov/common/leftmenu/EgovLeftNavAbout.jsx
@@ -9,10 +9,10 @@ function EgovLeftNavAbout() {
             <div className="inner">
                 <h2>사이트 소개</h2>
                 <ul className="menu4">
-                    <li><NavLink to={URL.ABOUT_SITE} activeClassName="cur">1소개</NavLink></li>
-                    <li><NavLink to={URL.ABOUT_HISTORY} activeClassName="cur">2연혁</NavLink></li>
-                    <li><NavLink to={URL.ABOUT_ORGANIZATION} activeClassName="cur">3조직소개</NavLink></li>
-                    <li><NavLink to={URL.ABOUT_LOCATION} activeClassName="cur">4찾아오시는 길</NavLink></li>
+                    <li><NavLink to={URL.ABOUT_SITE} activeClassName="cur">소개</NavLink></li>
+                    <li><NavLink to={URL.ABOUT_HISTORY} activeClassName="cur">연혁</NavLink></li>
+                    <li><NavLink to={URL.ABOUT_ORGANIZATION} activeClassName="cur">조직소개</NavLink></li>
+                    <li><NavLink to={URL.ABOUT_LOCATION} activeClassName="cur">찾아오시는 길</NavLink></li>
                 </ul>
             </div>
         </div>
